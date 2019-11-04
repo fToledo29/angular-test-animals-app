359c96030b274be388d2c97df409ff7cd711c27e
fToledo29 <fernando_29837@live.com>
2019-11-04 14:46:36 -0500

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 // Modules
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 // **Custom**
 import { Animals } from './animals/animals.module';
-
+// Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     AppRoutingModule,
+    BrowserModule,
+    FormsModule,
     Animals
   ],
   providers: [],
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-component',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css']
+})
+export class HomeComponent {
+  imgPath = 'https://cdn.jsdelivr.net/gh/fToledo29/angular-test-animals-app@master/src/assets/imgs/whatsapp-logo-encapsulated-postscript-the-start-button.jpg';
+  //'../../assets/imgs/logo-start-button.jpg'
+}
